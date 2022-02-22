e271b02a6b3ffc9235631459f6770331277664a6
ingjvilladiegot <ingsistemajvilladiegot@gmail.com>
2022-02-21 23:19:10 -0500

    se inicia proyecto con express ejs y ejs layout y se creo config para vercel deploy.

---
+++ index.js
@@ -0,0 +1,17 @@
+'use strict'
+import express from 'express'
+import expressLayouts from 'express-ejs-layouts'
+import dotenv from'dotenv'
+const app = express()
+dotenv.config()
+
+app.set('view engine', 'ejs')
+app.use(expressLayouts)
+
+
+app.get('/', (req,res) =>{
+    res.send('welcome.ejs')
+})
+
+
+app.listen(5000, () => console.log(`app listening on port 5000! `))
