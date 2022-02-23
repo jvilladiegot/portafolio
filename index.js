5a3125e9380864ea3540f21912acaa9a4f3d117c
ingjvilladiegot <ingsistemajvilladiegot@gmail.com>
2022-02-22 21:03:38 -0500

    res sendFile html.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict'
 import express from 'express'
 import expressLayouts from 'express-ejs-layouts'
-import dotenv from'dotenv'
+import dotenv from 'dotenv'
+import path from 'path'
 const app = express()
 dotenv.config()
 
@@ -9,9 +10,10 @@ app.set('view engine', 'ejs')
 app.use(expressLayouts)
 
 
-app.get('/', (req,res) =>{
-    res.send('welcome.ejs')
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/views/index.html'));
 })
+
 
 
 app.listen(5000, () => console.log(`app listening on port 5000! `))
