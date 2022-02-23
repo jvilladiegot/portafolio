445da7a330d6733bd9b175669d0f1124926e4a7c
ingjvilladiegot <ingsistemajvilladiegot@gmail.com>
2022-02-22 22:43:29 -0500

    se agrega __dirname a Module.

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 'use strict'
 import express from 'express';
 import { engine } from 'express-handlebars';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
     res.render('home');
