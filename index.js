5c3f49742926c5b099f0481abe60fcbe21aa6497
ingjvilladiegot <ingsistemajvilladiegot@gmail.com>
2022-02-22 23:07:42 -0500

    de deja funcionando con una tarjeta de profile .

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 'use strict'
-import express from 'express';
-import { engine } from 'express-handlebars';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import express from 'express'
+import { engine } from 'express-handlebars'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import axios from 'axios'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const app = express();
 
 app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars')
+app.set('views', __dirname + '/views')
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get('/', async (req, res) => {
+    const profile = {
+        email: "ingsistemajvilladiegot@gmail.com",
+        linkedin: 'jeaffer-villadiego-turizo',
+        github: 'ing27villadiego'
+    }
+    res.render('home', { profile })
 });
 
-app.listen(5000);
+app.listen(5000)
+
+console.log('Runnig as port 5000')
